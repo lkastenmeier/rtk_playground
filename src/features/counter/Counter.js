4359d23318de76721286c1a2e8f411bd36cb6425
Lena Kastenmeier <lenakastenmeier@Lenas-MacBook-Pro.local>
2023-08-30 13:39:29 +0200

    add reset and custom increment functionality

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, incrementByAmount } from './counterSlice';
+import { increment, decrement, incrementByAmount, reset } from './counterSlice';
 const Counter = () => {
     const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
+    const [incrementAmount, setIncrementAmount] = React.useState(0);
+
+
+    const resetAll = () => {
+        setIncrementAmount(0);
+        dispatch(reset());
+    }
+
     return (
         <section>
             <p>{count}</p>
@@ -21,6 +29,25 @@ const Counter = () => {
                 >
                     -
                 </button>
+                <button
+                    aria-label="Reset"
+                    onClick={() => resetAll()}
+                >
+                    Reset
+                </button>
+                <br />
+                <input
+                    value={incrementAmount}
+                    type="number"
+                    onChange={(e) => setIncrementAmount(e.target.value)}
+                />
+                <button
+                    aria-label="Increment by amount"
+                    onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+                >
+                    Add Amount
+                </button>
+
             </div>
         </section>
 
